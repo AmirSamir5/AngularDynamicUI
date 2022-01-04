2f3200a0e6276141829411ff9448164c63e67c99
AbdelRahman Tarek <atarek@citglobal.com>
2022-01-04 13:51:34 +0200

    draggable element

--- src/app/components/draggable-component/draggable-component.component.ts
+++ src/app/components/draggable-component/draggable-component.component.ts
@@ -1,19 +1,41 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { ElementModel } from 'src/app/models/element.model';
+import { DropdownComponent } from '../elements/dropdown/dropdown.component';
+import { TextComponent } from '../elements/text/text.component';
 
 @Component({
   selector: 'app-draggable-component',
   templateUrl: './draggable-component.component.html',
-  styleUrls: ['./draggable-component.component.css']
+  styleUrls: ['./draggable-component.component.css'],
 })
 export class DraggableComponentComponent implements OnInit {
   item = 'appbar title';
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  items: ElementModel[] = [
+    new ElementModel(TextComponent, ''),
+    new ElementModel(DropdownComponent, ''),
+    new ElementModel(TextComponent, ''),
+    new ElementModel(DropdownComponent, ''),
+    new ElementModel(TextComponent, ''),
+  ];
+  constructor() {}
 
-  editItem(item:string){
+  ngOnInit(): void {}
+
+  editItem(item: string) {
     this.item = item;
   }
 
+  drag(event: CdkDragDrop<ElementModel[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      this.items.splice(event.previousIndex, 1);
+    }
+  }
 }
