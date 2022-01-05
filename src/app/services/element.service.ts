c512dbefc88eb4fc7e57c91e6a562a289d376659
AbdelRahman Tarek <atarek@citglobal.com>
2022-01-05 14:06:10 +0200

    fix by reference item selection

--- src/app/services/element.service.ts
+++ src/app/services/element.service.ts
@@ -1,29 +1,35 @@
-import { EventEmitter } from "@angular/core";
-import { ElementModel } from "../models/element.model";
+import { EventEmitter } from '@angular/core';
+import { ElementModel } from '../models/element.model';
 
-export class ElementService{
-    elementChanged = new EventEmitter<ElementModel[]>();
+export class ElementService {
+  elementChanged = new EventEmitter<ElementModel[]>();
 
-    private elements:ElementModel[] = [
-       new ElementModel('Dropdown', 'Title', 'Dropdown',''),
-       new ElementModel('Textfield','Title','Textfield',''),
-       new ElementModel('Button','Button','',''),
-    ];
+  private elements: ElementModel[] = [
+    new ElementModel('Dropdown', 'Title', 'Dropdown', ''),
+    new ElementModel('Textfield', 'Title', 'Textfield', ''),
+    new ElementModel('Button', 'Button', '', ''),
+  ];
 
-    public selectedElements: ElementModel[] = [];
+  public selectedElements: ElementModel[] = [];
 
-    getElements(){
-        return this.elements.slice();
-    }
+  getElements() {
+    return this.elements.slice();
+  }
 
-    addSelectedItems(selectedElement:ElementModel){
-        console.log('Elements ' , this.elements);
-        this.selectedElements.push(selectedElement);
-        this.elementChanged.emit(this.selectedElements);
-    }
+  addSelectedItems(selectedElement: ElementModel) {
+    console.log('Elements ', this.elements);
+    var newRefSelectedElement = new ElementModel(
+      selectedElement.type,
+      selectedElement.title,
+      selectedElement.hintText,
+      selectedElement.json
+    );
+    this.selectedElements.push(newRefSelectedElement);
+    this.elementChanged.emit(this.selectedElements);
+  }
 
-    removeSelectedItems(index:number){
-        this.selectedElements.splice(index,1);
-        this.elementChanged.emit(this.selectedElements);
-    }
+  removeSelectedItems(index: number) {
+    this.selectedElements.splice(index, 1);
+    this.elementChanged.emit(this.selectedElements);
+  }
 }
