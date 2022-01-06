e0f9b1e6baa7b02216b2533879631a9f25cb32d2
AbdelRahman Tarek <atarek@citglobal.com>
2022-01-06 13:18:36 +0200

    input field properties UI

--- src/app/components/elements-properties/textfield-properties/textfield-properties.component.ts
+++ src/app/components/elements-properties/textfield-properties/textfield-properties.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { InputFieldElementModel } from 'src/app/models/input-field.model';
 
 @Component({
   selector: 'app-textfield-properties',
   templateUrl: './textfield-properties.component.html',
-  styleUrls: ['./textfield-properties.component.css']
+  styleUrls: ['./textfield-properties.component.css'],
 })
 export class TextfieldPropertiesComponent implements OnInit {
+  @Input() inputFieldElementModel: InputFieldElementModel =
+    new InputFieldElementModel(
+      'DropTextfielddown',
+      '',
+      '',
+      '',
+      false,
+      0,
+      0,
+      '',
+      '',
+      '',
+      '',
+      0
+    );
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.inputFieldElementModel);
   }
-
 }
