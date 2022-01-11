afbb5a7f9b4aa191533333b4ba0a4aed081d587d
AbdelRahman Tarek <atarek@citglobal.com>
2022-01-11 10:59:22 +0200

    dropdown data binding

--- src/app/components/elements/dropdown/dropdown.component.ts
+++ src/app/components/elements/dropdown/dropdown.component.ts
@@ -3,14 +3,19 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dropdown',
   templateUrl: './dropdown.component.html',
-  styleUrls: ['./dropdown.component.css']
+  styleUrls: ['./dropdown.component.css'],
 })
 export class DropdownComponent implements OnInit {
-  @Input() public title: string = 'Title';
-  @Input() public hintText: string = 'Dropdown';
-  constructor() { }
+  @Input() public title?: string;
+  @Input() public hintText?: string;
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.title === undefined) {
+      this.title = 'Title';
+    }
+    if (this.hintText === undefined) {
+      this.hintText = 'Dropdown';
+    }
   }
-
 }
