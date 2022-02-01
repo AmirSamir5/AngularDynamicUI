cca742e0a90fe6f627fe54a91965e15eae4a18bd
AbdelRahman Tarek <atarek@citglobal.com>
2022-02-01 13:36:26 +0200

    added apiCode to submit button

--- src/app/components/elements-properties/button-properties/button-properties.component.ts
+++ src/app/components/elements-properties/button-properties/button-properties.component.ts
@@ -6,29 +6,30 @@ import { ElementService } from 'src/app/services/element.service';
 @Component({
   selector: 'app-button-properties',
   templateUrl: './button-properties.component.html',
-  styleUrls: ['./button-properties.component.css']
+  styleUrls: ['./button-properties.component.css'],
 })
 export class ButtonPropertiesComponent implements OnInit {
   @Input() buttonItem?: WidgetModel;
-  @Input() index:number = 0;
-  @Input() buttonText:string = '';
-  
+  @Input() index: number = 0;
+  @Input() buttonText: string = '';
+  @Input() apiCode: string = '';
 
-  constructor(private elementService:ElementService) { }
+  constructor(private elementService: ElementService) {}
 
   ngOnInit(): void {
     this.buttonText = this.buttonItem!.fieldTitle ?? '';
+    this.apiCode = this.buttonItem!.apiCode ?? '';
   }
 
   onSubmit() {
-    if(this.buttonText === ''){
+    if (this.buttonText === '') {
       window.alert('Please Enter Title');
       return;
     }
     this.buttonItem!.widget_type = AppConstants.WIDGET_SUBMIT_BUTTON;
     this.buttonItem!.fieldTitle = this.buttonText;
-    this.elementService.onSaveItem(this.buttonItem!,this.index);
+    this.buttonItem!.apiCode = this.apiCode;
+    this.elementService.onSaveItem(this.buttonItem!, this.index);
     this.buttonText = '';
   }
-
 }
